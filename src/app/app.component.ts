206757800e779ac02d3b8fdf4f412ac6c5b824a3
albernalca <albernalca@gmail.com>
2023-03-28 13:23:49 -0500

    Ejemplo Event Divinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,33 +7,14 @@ import { Component,} from '@angular/core';
 })
 export class AppComponent {
 
-  //Property DataBiding: Caracteristica que comunica datos entre el TS y HTML
-
-  //INTERPOLACIÓN : los datos viajan desde el componente hacia el DOM 
-
-  //Declaro la Variable asignando un valor:
-  nombre: string = 'Alejandro'
-  deshabilitado: boolean = true
-
-  //Declaro variable 
-  textoPlaceholder= 'Escriba su nombre aqui'
-
-  //creo constructor utilizo una función interval de JS con la función flecha ()=>
-  constructor(){
-    setInterval( ()=> this.nombre='Juan Manuel',3000)
-    //Property databinding => creo función   
-    setInterval( ()=> this.deshabilitado = false , 3000)
-
-  }
-
-  //creo un metodo y recibe 2 parámetros de tipo number
-  getSuma(numero1:number, numero2:number){
-    return numero1 + numero2
-  }
+texto = 'Esto es Event biding'
+ 
+cambiarTexto(): void{
+  this.texto = "En el Click cambia de texto por Alejandro"
+}
 
  
  
   
 }
 
-//DATABINDING = Permite comunicar datos entre el archivo TS y HTML de forma unidireccional o bidireccional
