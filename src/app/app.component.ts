1b8fdda51fe94da0d11ebac121491c20f4b75b68
albernalca <albernalca@gmail.com>
2023-03-28 13:11:11 -0500

    Property databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,23 @@ import { Component,} from '@angular/core';
 })
 export class AppComponent {
 
-  //DataBiding: Caracteristica que comunica datos entre el TS y HTML de manera unidireccional y bidireccional
+  //Property DataBiding: Caracteristica que comunica datos entre el TS y HTML
 
   //INTERPOLACIÓN : los datos viajan desde el componente hacia el DOM 
 
   //Declaro la Variable asignando un valor:
   nombre: string = 'Alejandro'
+  deshabilitado: boolean = true
 
-  //creo constructor usutizo una función interval de JS con la función flecha ()=>
+  //Declaro variable 
+  textoPlaceholder= 'Escriba su nombre aqui'
+
+  //creo constructor utilizo una función interval de JS con la función flecha ()=>
   constructor(){
     setInterval( ()=> this.nombre='Juan Manuel',3000)
+    //Property databinding => creo función   
+    setInterval( ()=> this.deshabilitado = false , 3000)
+
   }
 
   //creo un metodo y recibe 2 parámetros de tipo number
